a76f7db869dbc8c19dc58c8d42acae42f353962f
Cosmin Nicolae <marnicos2006@yahoo.com>
2016-12-11 07:04:27 +0000

    Added home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,107 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v3.10, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.10, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('landing-assets/images/logo-314x128-90.png') }}" type="image/x-icon">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+  <link rel="stylesheet" href="{{ asset('landing-assets/bootstrap-material-design-font/css/material.css') }}">
+  <link rel="stylesheet" href="{{ asset('landing-assets/tether/tether.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('landing-assets/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('landing-assets/animate.css/animate.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('landing-assets/dropdown/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('landing-assets/socicon/css/socicon.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('landing-assets/theme/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('landing-assets/mobirise/css/mbr-additional.css') }}" type="text/css">
+  
+  
+  
+</head>
+<body>
+<section id="menu-1">
 
-                <div class="panel-body">
-                    You are logged in!
+    <nav class="navbar navbar-dropdown bg-color transparent navbar-fixed-top">
+        <div class="container">
+
+            <div class="mbr-table">
+                <div class="mbr-table-cell">
+
+                    <div class="navbar-brand">
+                        <a href="#" class="navbar-logo"><img src="{{ asset('landing-assets/images/logo-314x128-90.png')}} " alt="Mobirise"></a>
+                        
+                    </div>
+
+                </div>
+                <div class="mbr-table-cell">
+
+                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="hamburger-icon"></div>
+                    </button>
+
+                    <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar"><li class="nav-item dropdown"><a class="nav-link link" href="https://mobirise.com/" aria-expanded="false"></a></li><li class="nav-item nav-btn"><a class="nav-link btn btn-white-outline btn-white" href="https://mobirise.com/"><span style="font-size: 19.200000762939453px; font-weight: normal; line-height: 9.600000381469727px;">Logout</span></font></a></li></ul>
+                    <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="close-icon"></div>
+                    </button>
+
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+</section>
+
+<section class="engine"><a rel="external" href="https://mobirise.com">simple bootstrap web page maker</a></section><section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background mbr-section-with-arrow mbr-after-navbar" id="header1-4" style="background-image: url({{ asset('landing-assets/images/1a6cafd3a0a79e60e39b6bdff215dd67-2000x1250-41.jpg') }});">
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(0, 0, 0);"></div>
+
+    <div class="mbr-table-cell">
+
+        <div class="container">
+            <div class="row">
+                <div class="mbr-section col-md-10 col-md-offset-1 text-xs-center">
+
+                    <h1 class="mbr-section-title display-1">Welcome, !</h1>
+                    <p class="mbr-section-lead lead">Please search your airport and flight details in the form below</p>
+                    
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+    <div class="mbr-arrow mbr-arrow-floating" aria-hidden="true"><a href="#content1-5"><i class="mbr-arrow-icon"></i></a></div>
+
+</section>
+
+<section class="mbr-section article mbr-section__container" id="content1-5" style="background-color: rgb(255, 255, 255); padding-top: 20px; padding-bottom: 20px;">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 lead"><p>Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. MobiRise Website Builder creates responsive, retina and mobile friendly websites in a few clicks. Mobirise is one of the easiest website development tools <a href="http://google.com/">available</a> today. It also gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.</p></div>
+        </div>
+    </div>
+
+</section>
+
+
+    <script src="{{ asset('landing-assets/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('landing-assets/tether/tether.min.js') }}"></script>
+  <script src="{{ asset('landing-assets/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('landing-assets/smooth-scroll/SmoothScroll.js') }}"></script>
+  <script src="{{ asset('landing-assets/viewportChecker/jquery.viewportchecker.js') }}"></script>
+  <script src="{{ asset('landing-assets/dropdown/js/script.min.js') }}"></script>
+  <script src="{{ asset('landing-assets/touchSwipe/jquery.touchSwipe.min.js') }}"></script>
+  <script src="{{ asset('landing-assets/jarallax/jarallax.js') }}"></script>
+  <script src="{{ asset('landing-assets/theme/js/script.js') }}"></script>
+  
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>
